383197fa5ebb8710f281654caa312e50972bc189
tlamlert <tanadol_lamlertprasertkul@brown.edu>
2022-01-30 03:01:59 -0500

    old html

--- extension/run_model.js
+++
@@ -1,8 +0,0 @@
-var port = chrome.runtime.connectNative('com.my_company.my_application');
-port.onMessage.addListener(function(msg) {
-  console.log("Received" + msg);
-});
-port.onDisconnect.addListener(function() {
-  console.log("Disconnected");
-});
-port.postMessage({ text: "Hello, my_application" });
