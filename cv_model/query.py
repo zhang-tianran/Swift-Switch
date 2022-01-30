a2dc17358c818338b71049ec1433d6b4a7c839df
Alyssa Loo <alyssaloosg@gmail.com>
2022-01-30 09:34:38 -0500

    removed some comment on query

--- cv_model/query.py
+++ cv_model/query.py
@@ -1,8 +1,6 @@
 import functions
 import urllib.error
 
-# INPUT_IMAGE_URL = 'https://media1.popsugar-assets.com/files/thumbor/OZTN2C1c-Ur2QoMhOgKGvdJB7iE/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/01/24/981/n/1922398/shop08XKid/i/Shein-Sequin-Detail-Glitter-Mesh-Wrap-Dress.jpg'
-
 def query(input_url : str) -> list:
     db_embeddings = functions.load_db_embeddings()
     db_list = functions.load_db_list()
