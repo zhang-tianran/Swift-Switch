05df91a2d70340355d45c2ccecf97bdabe497c5d
tlamlert <tanadol_lamlertprasertkul@brown.edu>
2022-01-30 02:25:05 -0500

    initial commit

--- scrape_thredup.py
+++ scrape_thredup.py
@@ -24,56 +24,73 @@ def get_pages(driver, URL):
 
     return urls
 
-
-if __name__ == "__main__":
+def get_size_price(url):
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(options=options)
-
-    mainURL = "https://www.thredup.com/women/tops/t-shirts?department_tags=women&category_tags=tops&style_tags=t-shirts&sort=price_low_high&page="
-
-    for i in range(0, 40):
-        urls = get_pages(driver, mainURL+str(i+1))
-
-        url_img_src = []
-        for idx, url in enumerate(urls):
-            img_src = get_img_src(driver, url)
-            # print(idx, url, img_src)
-            url_img_src.append([url, img_src])
-
-        with open('products/db_'+str(i+1)+'.txt', 'w') as f:
-            csv.writer(f, delimiter=',').writerows(url_img_src)
-
+    driver.get(url)
+    # size = driver.find_elements(By.XPATH, '//div[@class="P9j6cGJ6kvC9bBgLk4pE"]')[0].get_attribute('innerHTML')
+    # price = driver.find_elements(By.XPATH, '//span[@class="u-text-20 u-font-bold u-text-alert price"]')[0].get_attribute('innerHTML')
+    size = driver.find_elements(By.XPATH, '//div[@class="P9j6cGJ6kvC9bBgLk4pE"]')[0].text
+    price = driver.find_elements(By.XPATH, '//span[@class="u-text-20 u-font-bold u-text-alert price"]')[0].text
     driver.quit()
+    print(size)
+    print(price)
+    return size, price
 
-    # # url_img_src = [['https://www.thredup.com/product/women-cotton-old-navy-green-long-sleeve-t-shirt/94262355?query_id=600579103082504192&result_id=600579103896199168', 'https://cf-assets-thredup.thredup.com/assets/283097661/retina.jpg'], ['https://www.thredup.com/product/women-spandex-j-peterman-orange-short-sleeve-t-shirt/104176634?query_id=600579103082504192&result_id=600579103896199168', 'https://cf-assets-thredup.thredup.com/assets/314437058/retina.jpg']]
-    # # print(url_img_src)
-    #
+get_size_price("https://www.thredup.com/product/women-cotton-st-johns-bay-brown-long-sleeve-t-shirt/115572825?query_id=600668285909860352&result_id=600668287331729408")
 
-    # with open('first_10.txt', 'r') as f:
-    #     reader = csv.reader(f, delimiter=',')
-    #     for row in reader:
-    #         print(row)
-
-    # a = np.array([1, 2, 3, 4])
-    # np.savetxt('first_10.txt', url_img_src, fmt='%s')
-    # b = np.loadtxt('first_10.txt', dtype='object')
-    # print( url_img_src == b )
-
-    # URL1 = "https://www.thredup.com/product/women-cotton-madewell-black-long-sleeve-t-shirt/115320641?query_id=600519482275831808&result_id=600519484045828096"
-    # URL2 = "https://www.thredup.com/product/women-cotton-old-navy-green-long-sleeve-t-shirt/94262355?query_id=600568322274131968&result_id=600568323201073152"
-    #
-    # options = webdriver.ChromeOptions()
-    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    # options.add_experimental_option('useAutomationExtension', False)
-    # options.add_argument("--disable-blink-features=AutomationControlled")
-    # driver = webdriver.Chrome(options=options)
-    #
-    # img_src1 = get_img_src(driver, URL1)
-    # img_src2 = get_img_src(driver, URL2)
-    # print(img_src1)
-    # print(img_src2)
-    #
-    # driver.quit()
+# if __name__ == "__main__":
+#     options = webdriver.ChromeOptions()
+#     options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#     options.add_experimental_option('useAutomationExtension', False)
+#     options.add_argument("--disable-blink-features=AutomationControlled")
+#     driver = webdriver.Chrome(options=options)
+#
+#     mainURL = "https://www.thredup.com/women/tops/t-shirts?department_tags=women&category_tags=tops&style_tags=t-shirts&sort=price_low_high&page="
+#
+#     for i in range(0, 40):
+#         urls = get_pages(driver, mainURL+str(i+1))
+#
+#         url_img_src = []
+#         for idx, url in enumerate(urls):
+#             img_src = get_img_src(driver, url)
+#             # print(idx, url, img_src)
+#             url_img_src.append([url, img_src])
+#
+#         with open('products/db_'+str(i+1)+'.txt', 'w') as f:
+#             csv.writer(f, delimiter=',').writerows(url_img_src)
+#
+#     driver.quit()
+#
+#     # # url_img_src = [['https://www.thredup.com/product/women-cotton-old-navy-green-long-sleeve-t-shirt/94262355?query_id=600579103082504192&result_id=600579103896199168', 'https://cf-assets-thredup.thredup.com/assets/283097661/retina.jpg'], ['https://www.thredup.com/product/women-spandex-j-peterman-orange-short-sleeve-t-shirt/104176634?query_id=600579103082504192&result_id=600579103896199168', 'https://cf-assets-thredup.thredup.com/assets/314437058/retina.jpg']]
+#     # # print(url_img_src)
+#     #
+#
+#     # with open('first_10.txt', 'r') as f:
+#     #     reader = csv.reader(f, delimiter=',')
+#     #     for row in reader:
+#     #         print(row)
+#
+#     # a = np.array([1, 2, 3, 4])
+#     # np.savetxt('first_10.txt', url_img_src, fmt='%s')
+#     # b = np.loadtxt('first_10.txt', dtype='object')
+#     # print( url_img_src == b )
+#
+#     # URL1 = "https://www.thredup.com/product/women-cotton-madewell-black-long-sleeve-t-shirt/115320641?query_id=600519482275831808&result_id=600519484045828096"
+#     # URL2 = "https://www.thredup.com/product/women-cotton-old-navy-green-long-sleeve-t-shirt/94262355?query_id=600568322274131968&result_id=600568323201073152"
+#     #
+#     # options = webdriver.ChromeOptions()
+#     # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#     # options.add_experimental_option('useAutomationExtension', False)
+#     # options.add_argument("--disable-blink-features=AutomationControlled")
+#     # driver = webdriver.Chrome(options=options)
+#     #
+#     # img_src1 = get_img_src(driver, URL1)
+#     # img_src2 = get_img_src(driver, URL2)
+#     # print(img_src1)
+#     # print(img_src2)
+#     #
+#     # driver.quit()
